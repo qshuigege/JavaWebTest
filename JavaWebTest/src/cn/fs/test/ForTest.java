f43a04b068aa66d07c13553be0afdf1af5cab733
qshuigege <qshuigege@163.com>
2016-01-05 21:41:27 +0800

    aaa

--- JavaWebTest/src/cn/fs/test/ForTest.java
+++ JavaWebTest/src/cn/fs/test/ForTest.java
@@ -4,5 +4,6 @@ public class ForTest {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 		System.out.println("hello,hello!");
+		System.out.println("hello,hello!");
 	}
 }
